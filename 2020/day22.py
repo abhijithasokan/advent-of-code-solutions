9b9199db258a4ca147d255502cabc2aa91aa1072
abhijithasokan <abhijithasokan27@gmail.com>
2020-12-22 21:33:50 +0530

    Add solution for 2020/day-22

--- 2020/day22.py
+++ 2020/day22.py
@@ -1,130 +1,114 @@
 from utils import aoc_comm
 import os
-import re
 
-# --- update day/ year for each challenge
+# --- AUTO SUBMIT SETTINGS ---
 settings = {
     'day' : 22,
     'year' : 2020,
     'cookie-path' : os.path.realpath('../aoc_cookie.json')
 }
 
-from collections import Counter, defaultdict
-import math
-import functools
-import itertools
-
-
 
 def parse_input(inp_content):
     inp_content = inp_content.strip().split('\n\n')
-    p1, p2 = inp_content
-    p1, p2 = p1.split('\n')[1:], p2.split('\n')[1:]
-    return list(map(int, p1))[::-1], list(map(int, p2))[::-1]
+    p1_deck, p2_deck = inp_content
+    p1_deck, p2_deck = p1_deck.split('\n')[1:], p2_deck.split('\n')[1:]
+    return list(map(int, p1_deck))[::-1], list(map(int, p2_deck))[::-1]
 
 
-
-def game_l1(p1, p2):
-    while p1 and p2:
-        if p1[-1] > p2[-1]:
-            card1 = p1.pop()
-            card2 = p2.pop()
-            p1 = [card2, card1] + p1
+def game_l1(p1_deck, p2_deck):
+    while p1_deck and p2_deck:
+        card1 = p1_deck.pop()
+        card2 = p2_deck.pop()
+        if card1 > card2:
+            p1_deck = [card2, card1] + p1_deck
         else:
-            card1 = p1.pop()
-            card2 = p2.pop()
-            p2 = [card1, card2] + p2
-    return p1, p2
-
-def get_final_num(pp):
-    return sum( (ind + 1) * ee for ind, ee in enumerate(pp)  )
-
-@aoc_comm(settings, level = 1)
-def solve_l1(input_str): 
-    p1, p2 = parse_input(input_str)
-    p1, p2 = game_l1(p1, p2)
-    return get_final_num(p1 or p2) 
+            p2_deck = [card1, card2] + p2_deck
+    return p1_deck, p2_deck
 
 
+def get_final_num(deck):
+    return sum( (ind + 1) * card for ind, card in enumerate(deck)  )
 
-def check_cache(keyy, memo_table):
-    if keyy in memo_table:
-        print("cache hit")
-        return memo_table[keyy]
+def check_cache(decks, cache_table):
+    if decks in cache_table:
+        #print("cache hit")
+        return cache_table[decks]
 
-    keyy = keyy[::-1]
-    if keyy in memo_table:
-        print("flip cache hit")
-        return 1 - memo_table[keyy]
+    exhcanged_decks = decks[::-1]
+    if exhcanged_decks in cache_table:
+        #print("flip cache hit")
+        return 1 - cache_table[exhcanged_decks]  # other player wins ( if winner is 1, winner after exhange is 2 (which is represented as 0) )
     return None
 
 
-import copy
-def game_l2(p1, p2, memo_table, sub = True):
-    if sub:
-        max_card1 = max(p1)
-        max_card2 = max(p2)
-        if max_card1 > max_card2  and ( max_card1 > (len(p1) + len(p2)-2) ):
+CALLS_TO_FUNC = 0
+def game_l2(p1_deck, p2_deck, cache_table, is_sub_game = True):
+    if is_sub_game:
+        max_card1 = max(p1_deck)
+        max_card2 = max(p2_deck)
+        if max_card1 > max_card2 and (max_card1 > (len(p1_deck) + len(p2_deck) - 2) ):
+            #player 1 wins!
             return 1, None, None
 
-    cache_table_key = (tuple(p1), tuple(p1))
-    cache_entry = check_cache(cache_table_key, memo_table)
+    cache_table_key = (tuple(p1_deck), tuple(p2_deck))
+    final_winner = check_cache(cache_table_key, cache_table)
     
-    if cache_entry is not None:
-        return cache_entry, None, None
-    
-    seen_deck1, seen_deck2 = set(), set()
+    if final_winner is not None:
+        return final_winner, None, None
 
-    intermediate_states = []
-    while p1 and p2:
-        deck1 = tuple(p1)
-        deck2 = tuple(p2)
-        
-        if deck1 in seen_deck1 or deck2 in seen_deck2:
+    global CALLS_TO_FUNC
+    CALLS_TO_FUNC += 1
+    
+    seen_decks = set()
+    
+    while p1_deck and p2_deck:
+        decks = (tuple(p1_deck), tuple(p2_deck))
+
+        if decks in seen_decks:
             final_winner = 1
-            memo_table[cache_table_key] = final_winner
+            cache_table[cache_table_key] = final_winner
             return final_winner, None, None
 
-        seen_deck1.add(deck1)
-        seen_deck2.add(deck2)
-        intermediate_state = (deck1, deck2)
-        intermediate_states.append(intermediate_state)
-
-        cache_entry = check_cache(intermediate_state, memo_table)    
+        seen_decks.add(decks)
+        
+        cache_entry = check_cache(decks, cache_table)    
         if cache_entry is not None:
             return cache_entry, None, None
-        
-        if p1[-1] <= (len(p1)-1) and p2[-1] <= (len(p2)-1):
-            len1 = p1[-1]
-            len2 = p2[-1]
-            new_p1, new_p2  = copy.deepcopy(p1), copy.deepcopy(p2)
-            new_p1.pop(), new_p2.pop()
-            winner, _ ,_  = game_l2( new_p1[-len1:], new_p2[-len2:], memo_table)
+
+        card1 = p1_deck.pop()
+        card2 = p2_deck.pop()
+        if card1 <= len(p1_deck) and card2 <= len(p2_deck):
+            new_p1_deck, new_p2_deck  = list(p1_deck[-card1:]), list(p2_deck[-card2:])
+            round_winner, _ ,_  = game_l2(new_p1_deck, new_p2_deck, cache_table, True)
         else:
-            winner = 1 if p1[-1] > p2[-1] else 0
+            round_winner = 1 if card1 > card2 else 0
             
-        if winner == 1:
-            card1 = p1.pop()
-            card2 = p2.pop()
-            p1 = [card2, card1] + p1
+        if round_winner == 1:
+            p1_deck = [card2, card1] + p1_deck
         else:
-            card1 = p1.pop()
-            card2 = p2.pop()
-            p2 = [card1, card2] + p2
+            p2_deck = [card1, card2] + p2_deck
             
-    final_winner = 1 if len(p1) else 0
-    for state in intermediate_states:
-        memo_table[state] = final_winner
-        state = state[::-1]
-        memo_table[state] = 1 - final_winner
-        
-    return final_winner, p1, p2
-    
+    final_winner = 1 if p1_deck else 0
+    cache_table[cache_table_key] = final_winner
+    return final_winner, p1_deck, p2_deck
+
+
+
+
+@aoc_comm(settings, level = 1)
+def solve_l1(input_str): 
+    p1_deck, p2_deck = parse_input(input_str)
+    p1_deck, p2_deck = game_l1(p1_deck, p2_deck)
+    return get_final_num(p1_deck or  p2_deck)
+
+
 @aoc_comm(settings, level = 2)
 def solve_l2(input_str):
-    p1, p2 = parse_input(input_str)
-    winner, p1, p2 = game_l2(p1, p2, {}, False)
-    return get_final_num(p1 if winner == 1 else p2)
+    p1_deck, p2_deck = parse_input(input_str)
+    winner, p1_deck, p2_deck = game_l2(p1_deck, p2_deck, {}, False)
+    print("CALLS_TO_FUNC: " , CALLS_TO_FUNC)
+    return get_final_num(p1_deck if winner == 1 else p2_deck)
 
 
 def main():
